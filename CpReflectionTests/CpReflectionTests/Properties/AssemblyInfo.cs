0088255cdd27d7c5cb9237faf0bf5e66f1d52dc3
Mikhail Naumov <mikhail_naumov@yahoo.com>
2019-04-05 20:07:02 -0700

    First commit

---
+++ CpReflectionTests/CpReflectionTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CpReflectionTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CpReflectionTests")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("111668c2-9a9b-4189-a95c-585716bf629a")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ CpReflectionTests/CpReflectionTests/Tests.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace cpGames.core.CpReflection.Tests
+{
+    [TestClass]
+    public class Tests
+    {
+        #region Methods
+        [TestMethod]
+        public void TestReflection()
+        {
+            // ToDo someday
+        }
+        #endregion
+    }
+}
