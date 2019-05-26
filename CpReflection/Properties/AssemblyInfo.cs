0663780a91a223376fffa398507d26f4edf0a124
Mikhail Naumov <mikhail_naumov@yahoo.com>
2019-05-26 12:36:16 -0700

    Restructured solution

--- CpReflection/Properties/AssemblyInfo.cs
+++ CpReflection/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("427fe9b3-08bb-4f31-bd3e-a560c2b4d718")]
+[assembly: Guid("8cf66cec-4538-4f56-96ba-37c07604d65c")]
 
 // Version information for an assembly consists of the following four values:
 //
--- Tests/Properties/AssemblyInfo.cs
+++ Tests/Properties/AssemblyInfo.cs
@@ -2,18 +2,18 @@ using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("CpReflectionTests")]
+[assembly: AssemblyTitle("Tests")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("CpReflectionTests")]
+[assembly: AssemblyProduct("Tests")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
 [assembly: ComVisible(false)]
 
-[assembly: Guid("111668c2-9a9b-4189-a95c-585716bf629a")]
+[assembly: Guid("a6b96a7e-22b2-46b2-b7fc-11218fe3a3e5")]
 
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
